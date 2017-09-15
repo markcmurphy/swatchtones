62889fe61b8d83a6c4936945d00f0b4491b48537
Mark Murphy <mark@murphymark.me>
2017-09-15 15:38:45 -0500

    confirmed angular is working

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+app.use(express.static('public'));
+
 
 const productsController = require('./controllers/products.js');
 app.use('/products', productsController);
+
+
+
 
 mongoose.connect('mongodb://localhost:27017/swatches');
 mongoose.connection.once('open', ()=> {
   console.log('connected to mongoose..');
 });
+
 app.listen(3000, ()=> {
   console.log('listening on port 3000');
 });
