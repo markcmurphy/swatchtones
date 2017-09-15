dab9fffb095a3ce77b072197949081d794fcc8de
Mark Murphy <mark@murphymark.me>
2017-09-15 16:29:13 -0500

    am able to display info from makeup API on index

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,22 @@
 const app = angular.module('MyApp', []);
 console.log('app.js');
-app.controller('mainController', function(){
 
-});
+app.controller('mainController', ['$http', function($http) {
+  const controller = this;
+  this.products = {};
+
+  this.getProducts = () => {
+    $http({
+      method: 'GET',
+      url: 'http://makeup-api.herokuapp.com/api/v1/products.json'
+    }).then(response => {
+      this.products = response.data;
+    })
+    .catch(err => console.log(err));
+    console.log('error');
+  }
+
+  this.getProducts();
+
+//  end of mainController
+}]);
