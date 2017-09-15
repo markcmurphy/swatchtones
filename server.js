a1da14d4960e6d53451184e4642cfe2b68ce195b
Mark Murphy <mark@murphymark.me>
2017-09-15 15:21:34 -0500

    connected mongoose

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 
 const productsController = require('./controllers/products.js');
 app.use('/products', productsController);
 
+mongoose.connect('mongodb://localhost:27017/swatches');
+mongoose.connection.once('open', ()=> {
+  console.log('connected to mongoose..');
+});
 app.listen(3000, ()=> {
   console.log('listening on port 3000');
 });
