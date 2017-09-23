df8e82ec20dc3fb1138ab05d255968a82df6f2b2
Mark Murphy <mark@murphymark.me>
2017-09-23 15:00:40 -0500

    added logout method

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,18 @@
-const app = angular.module('MyApp', ['angular.filter','angularFileUpload']);
+const app = angular.module('MyApp', ['angular.filter', 'angularFileUpload']);
 
 app.controller('AppController', function($scope, FileUploader) {
-        $scope.uploader = new FileUploader({
-          url: 'https://api.imgur.com/3/image',
-          alias: 'image',
-          headers: {'Authorization': 'Client-ID dbae59ffd91b31d'},
-          autoUpload: true
-        }),
-        $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
-            console.info('onSuccessItem', fileItem, response, status, headers);
-        };
-      });
+  $scope.uploader = new FileUploader({
+      url: 'https://api.imgur.com/3/image',
+      alias: 'image',
+      headers: {
+        'Authorization': 'Client-ID dbae59ffd91b31d'
+      },
+      autoUpload: true
+    }),
+    $scope.uploader.onSuccessItem = function(fileItem, response, status, headers) {
+      console.info('onSuccessItem', fileItem, response, status, headers);
+    };
+});
 
 app.controller('mainController', ['$http', function($http) {
   const controller = this;
@@ -21,138 +23,138 @@ app.controller('mainController', ['$http', function($http) {
   this.values = {};
   this.rgb = {};
 
-// get swatches
+  // get swatches
   this.getSwatches = () => {
     $http({
-      method: 'GET',
-      url: '/swatches'
-    }).then(response => {
-      console.log('get swatches ran');
-      this.swatches = response.data;
+        method: 'GET',
+        url: '/swatches'
+      }).then(response => {
+        console.log('get swatches ran');
+        this.swatches = response.data;
 
-    })
-    .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
   }
 
   this.getSwatchColor = (swatch) => {
     $http({
-      method: 'GET',
-      url: '/swatches/' + swatch._id
-    }).then(response => {
-      res.json(response);
-    })
-    .catch(err => console.log(err));
+        method: 'GET',
+        url: '/swatches/' + swatch._id
+      }).then(response => {
+        res.json(response);
+      })
+      .catch(err => console.log(err));
   }
 
-// get products
+  // get products
   this.getProducts = () => {
     $http({
-      method: 'GET',
-      url: '/products'
-    }).then(response => {
-      this.products = response.data;
-    })
-    .catch(err => console.log(err));
+        method: 'GET',
+        url: '/products'
+      }).then(response => {
+        this.products = response.data;
+      })
+      .catch(err => console.log(err));
   }
 
-// edit product
+  // edit product
   this.editProduct = function(product) {
-     $http({
-       method: 'PUT',
-       url: '/products/' + product._id,
-       data: {
-         Id: this._id,
-         productName: this.productName,
-         productBrand: this.productBrand
-       }
-     }).then(
-       function(res) {
-         controller.getProducts();
-         controller.getSwatches();
-       },
-       function(err) {
-         console.log(err);
-       }
-     );
-   },
-
-// delete swatch
-  this.deleteSwatch = (swatch) => {
-  $http({
-    method: 'DELETE',
-    url: '/swatches/' + swatch._id
-  }).then(
-    function(res) {
-      controller.getSwatches();
+      $http({
+        method: 'PUT',
+        url: '/products/' + product._id,
+        data: {
+          Id: this._id,
+          productName: this.productName,
+          productBrand: this.productBrand
+        }
+      }).then(
+        function(res) {
+          controller.getProducts();
+          controller.getSwatches();
+        },
+        function(err) {
+          console.log(err);
+        }
+      );
     },
-    function(err) {
-      console.log(err);
-    }
-  );
-},
 
-// delete product
-  this.deleteProduct = (product) => {
-  $http({
-    method: 'DELETE',
-    url: '/products/' + product._id
-  }).then(
-    function(res) {
-      controller.getProducts();
-      controller.getSwatches();
+    // delete swatch
+    this.deleteSwatch = (swatch) => {
+      $http({
+        method: 'DELETE',
+        url: '/swatches/' + swatch._id
+      }).then(
+        function(res) {
+          controller.getSwatches();
+        },
+        function(err) {
+          console.log(err);
+        }
+      );
     },
-    function(err) {
-      console.log(err);
-    }
-  );
-},
 
-// form submit for products
-  this.processForm = function() {
-    $http({
-      method: 'POST',
-      url: '/products',
-      data: {
-        Id: this._id,
-        name: this.formdata.name,
-        brand: this.formdata.brand
-      }
-    }).then(response => {
-      console.log(response.data);
-      controller.formdata = {};
-      this.getProducts();
-    })
-    .catch(err => console.log(err));
-  }
+    // delete product
+    this.deleteProduct = (product) => {
+      $http({
+        method: 'DELETE',
+        url: '/products/' + product._id
+      }).then(
+        function(res) {
+          controller.getProducts();
+          controller.getSwatches();
+        },
+        function(err) {
+          console.log(err);
+        }
+      );
+    },
+
+    // form submit for products
+    this.processForm = function() {
+      $http({
+          method: 'POST',
+          url: '/products',
+          data: {
+            Id: this._id,
+            name: this.formdata.name,
+            brand: this.formdata.brand
+          }
+        }).then(response => {
+          console.log(response.data);
+          controller.formdata = {};
+          this.getProducts();
+        })
+        .catch(err => console.log(err));
+    }
 
   // form submit for swatches
   this.processSwatch = () => {
     $http({
-      method: 'POST',
-      url: '/swatches',
-      data: {
-        Id: this._id,
-        product: this.product,
-        imageLink: this.formdata.imageLink,
-        productColor: this.productColor,
-        skinTone: this.formdata.skinTone,
-        productId: this.formdata.productId
-      }
-    }).then(response => {
-      console.log(response.data);
-      controller.formdata = {};
-      this.getSwatches();
-    })
-    .catch(err => console.log(err));
+        method: 'POST',
+        url: '/swatches',
+        data: {
+          Id: this._id,
+          product: this.product,
+          imageLink: this.formdata.imageLink,
+          productColor: this.productColor,
+          skinTone: this.formdata.skinTone,
+          productId: this.formdata.productId
+        }
+      }).then(response => {
+        console.log(response.data);
+        controller.formdata = {};
+        this.getSwatches();
+      })
+      .catch(err => console.log(err));
   }
 
-this.getSwatches();
-this.getProducts();
+  this.getSwatches();
+  this.getProducts();
 
-//  end of mainController
+  //  end of mainController
 }]);
 
-app.controller('LoginModalCtrl', function ($http) {
+app.controller('LoginModalCtrl', function($http) {
   const controller = this;
   this.user = [];
 
@@ -160,54 +162,61 @@ app.controller('LoginModalCtrl', function ($http) {
     if (req.user) {
       next();
     } else {
-      return res.status(401).json({ message: 'Unauthorized user!' });
+      return res.status(401).json({
+        message: 'Unauthorized user!'
+      });
     }
   };
 
-  this.create = function(){
-    $http({
-      method: 'POST',
-      url: '/sessions/register',
-      data: {
-        email: this.email,
-        password: this.password
-      }
-    }
-    )
-},
+  this.create = function() {
+      $http({
+        method: 'POST',
+        url: '/sessions/register',
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      })
+    },
 
-  this.login = function(){
-    $http({
-      method: 'POST',
-      url: '/sessions/login',
-      data: {
-        email: this.email,
-        password: this.password
-      }}).then(
-          function(response) {
-            // console.log(response.data.foundUser.email);
-            this.user = response.data.foundUser;
-            console.log(this.user);
-            localStorage.setItem('token', JSON.stringify(response.data.token));
-          },
+    this.login = function() {
+      $http({
+        method: 'POST',
+        url: '/sessions/login',
+        data: {
+          email: this.email,
+          password: this.password
+        }
+      }).then(
+        function(response) {
+          // console.log(response.data.foundUser.email);
+          this.user = response.data.foundUser;
+          console.log(this.user);
+          localStorage.setItem('token', JSON.stringify(response.data.token));
+        },
         function(err) {
           console.log(err);
         }
       );
-},
+    },
 
-this.getUsers = function() {
-  $http({
-    method: 'GET',
-    url: '/users',
-    headers: {
-      token: JSON.parse(localStorage.getItem('token'))
+    this.logout = function() {
+      localStorage.clear('token');
+      location.reload();
     }
-  }).then(function(response) {
-    console.log("worked");
-    this.error = "Unauthorized";
-  }.bind(this));
-}
 
-// end of LoginModalCtrl
+    this.getUsers = function() {
+      $http({
+        method: 'GET',
+        url: '/users',
+        headers: {
+          token: JSON.parse(localStorage.getItem('token'))
+        }
+      }).then(function(response) {
+        console.log("worked");
+        this.error = "Unauthorized";
+      }.bind(this));
+    }
+
+  // end of LoginModalCtrl
 });
